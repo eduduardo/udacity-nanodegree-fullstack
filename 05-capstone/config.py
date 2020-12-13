2c4be8eeade2f8a31b790b59f039b40be2577668
Eduardo Ramos <eduardo.auramos@gmail.com>
2020-12-13 12:18:51 -0300

    heroku configs

--- 05-capstone/config.py
+++ 05-capstone/config.py
@@ -3,23 +3,13 @@
 #----------------------------------------------------------------------------#
 from os import environ
 from flask_sqlalchemy import SQLAlchemy
-from flask_migrate import Migrate
-
-# Configs to connect to the database
-database_host = environ.get('DATABASE_HOST', "localhost:5432")
-database_name = environ.get('DATABASE_NAME', "casting_agency")
-database_user = environ.get('DATABASE_USER', "app_user")
-database_path = "postgresql://{}@{}/{}".format(
-    database_user, database_host, database_name)
 
 db = SQLAlchemy()
 
-# Binds a Flask application with the SQLAlchemy service and Flask Migrate
-
+# Binds a Flask application with the SQLAlchemy service
 
 def setup_db(app, database_path=database_path):
-    app.config["SQLALCHEMY_DATABASE_URI"] = database_path
+    app.config["SQLALCHEMY_DATABASE_URI"] = environ.get('DATABASE_URL')
     app.config["SQLALCHEMY_TRACK_MODIFICATIONS"] = False
     db.app = app
     db.init_app(app)
-    migrate = Migrate(app, db)
